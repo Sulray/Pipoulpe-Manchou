0e26e755e72ad9105379a2e549e01c5638b6c51c
sulray <sulray250@gmail.com>
2022-05-17 13:05:15 +0200

    new material

---
+++ Assets/Scripts/Characters/ManchouMovement.cs
@@ -0,0 +1,176 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class ManchouMovement : MonoBehaviour
+{
+    Rigidbody2D rb;
+    private float inputX;
+    private float inputY;
+
+    [SerializeField] private float move_speed;
+    [SerializeField] private float jump_speed;
+    [SerializeField] private float swimBounceX;
+    [SerializeField] private float swimBounceY;
+    [SerializeField] private float gravity;
+    [SerializeField] private float swimGravity;
+    [SerializeField] private float drag;
+    [SerializeField] private float swimDrag;
+
+
+    LayerMask maskPlatform;
+    LayerMask maskPipoulpe;
+    LayerMask maskIce;
+    LayerMask maskWater;
+    LayerMask maskBoostJump;
+
+    private bool isOnPlatform = true;
+    private bool isOnPipoulpe = false;
+    private bool isOnIce = false;
+    private bool isInWater = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+        maskPlatform = LayerMask.GetMask("Platform");
+        maskPipoulpe = LayerMask.GetMask("Pipoulpe");
+        maskIce = LayerMask.GetMask("Ice");
+        maskWater = LayerMask.GetMask("Water");
+        maskBoostJump = LayerMask.GetMask("BoostJumpLayer");
+        rb.gravityScale = gravity;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isOnPlatform = CheckGround(maskPlatform);
+        isOnPipoulpe = CheckGround(maskBoostJump);
+        isOnIce = CheckGround(maskIce);
+        isInWater = CheckGround(maskWater);
+        //Debug.Log(isOnPipoulpe);
+
+        if (isInWater)
+        {
+            //rb.velocity = new Vector2(inputX * swim_speed, inputY * swim_speed);
+            //rb.AddForce(new Vector2(inputX, inputY) * swimBounce, ForceMode2D.Impulse);
+            rb.AddForce(new Vector2(inputX * swimBounceX, inputY * swimBounceY), ForceMode2D.Force);
+
+        }
+        else
+        {
+            rb.velocity = new Vector2(inputX * move_speed, rb.velocity.y);
+        }
+
+        if (isOnPipoulpe)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jump_speed);
+            rb.AddForce(new Vector2(5, 10), ForceMode2D.Impulse);
+            print("Add force");
+        }
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+
+        inputX = context.ReadValue<Vector2>().x;
+        if (isInWater)
+        {
+            Debug.Log("move isinwater");
+            inputY = context.ReadValue<Vector2>().y;
+            rb.AddForce(new Vector2(inputX * swimBounceX, inputY * swimBounceY) , ForceMode2D.Impulse);
+        }
+        else
+        {
+            inputY = 0;
+        }
+    }
+
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        //print("d�but jump");
+        if ((isOnPlatform)||(isOnIce))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jump_speed);
+        }
+        
+        /*
+        if (isOnPipoulpe)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jump_speed);
+            rb.AddForce(new Vector2(5, 10), ForceMode2D.Impulse);
+            print("Add force");
+        }
+        //print("fin jump");
+        */
+    }
+
+
+
+    void OnTriggerEnter2D(Collider2D other)     
+    {
+        /*
+        LayerMask otherLayer = other.gameObject.layer;
+
+        Debug.Log(other.gameObject.layer);
+        Debug.Log("other layer : " + otherLayer);
+        Debug.Log("mask water : " + maskWater);
+       
+        print(other.gameObject.layer);
+        Debug.Log("collided");
+        if (otherLayer == maskWater)
+        {
+            Debug.Log("Collided with water");
+        }
+        if (other.gameObject.layer == maskBoostJump)
+        {
+            Debug.Log("Collided with maskBoostJump");
+            Debug.Log(CheckGround(maskBoostJump));
+        }
+        */
+
+        if (other.gameObject.tag == "Water")
+        {
+            Debug.Log("enter water");
+            rb.gravityScale = swimGravity;
+            rb.drag = swimDrag;
+
+        }
+    }
+
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Water")
+        {
+            Debug.Log("exit water");
+            rb.gravityScale = gravity;
+            rb.drag = drag;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Pipoulpe")
+        {
+            Physics2D.IgnoreCollision(collision.gameObject.GetComponent<Collider2D>(), this.GetComponent<Collider2D>());
+        }
+    }
+
+    private bool CheckGround(LayerMask mask)
+    {
+        // print("Checkground");
+        BoxCollider2D collision = this.GetComponent<BoxCollider2D>();
+        RaycastHit2D rc = Physics2D.CircleCast(new Vector2(rb.position.x, rb.position.y), collision.size.x * transform.lossyScale.x / 2, new Vector2(0, -1), collision.size.y * transform.lossyScale.y * (1f / 2 + 1 / 10),mask);
+        return rc.collider != null;
+    }
+
+}
+
+
+
---
+++ Assets/Scripts/Characters/PipoulpeGrappler.cs
@@ -0,0 +1,156 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PipoulpeGrappler : MonoBehaviour
+{
+    public LineRenderer lineRenderer;
+    public DistanceJoint2D distanceJoint;
+    public SpringJoint2D springJoint;
+    Transform tf;
+    Rigidbody2D rb;
+
+    //private Vector2[] positions = new Vector2[10];
+    
+    public GameObject cursor;
+    Transform cursorTf;
+
+    [SerializeField] float boostForce;
+    public float aboveY;
+    public float speedForSpring;
+
+
+    private int indexPoint = 0;
+    private int numberOfPoints;
+    private List<Vector2> positions = new List<Vector2>();
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tf = this.GetComponent<Transform>();
+        rb = this.GetComponent<Rigidbody2D>();
+
+        cursorTf = cursor.GetComponent<Transform>();
+
+        GameObject[] GrapplingPoints = GameObject.FindGameObjectsWithTag("GrapplingPoint");
+        numberOfPoints = GrapplingPoints.Length;
+
+        if (numberOfPoints>0)
+        {
+            foreach(GameObject Point in GrapplingPoints)
+            {
+                positions.Add(Point.transform.position);
+            }
+
+            Debug.Log(numberOfPoints);
+            Debug.Log(positions[0]);
+            Debug.Log("index point = " + indexPoint);
+            cursorTf.position = positions[indexPoint] + new Vector2(0, aboveY);
+        }
+
+        distanceJoint.enabled = false;
+        lineRenderer.enabled = false;
+        springJoint.enabled = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (lineRenderer.enabled == true)
+        {
+            lineRenderer.SetPosition(0, tf.position);
+            if (rb.velocity.x > 0)
+            {
+                rb.AddForce(new Vector2(boostForce, 0),ForceMode2D.Force);
+            }
+            else if (rb.velocity.x<0)
+            {
+                rb.AddForce(new Vector2(-boostForce, 0), ForceMode2D.Force);
+            }
+        }
+
+    }
+
+
+    public void Grappling(InputAction.CallbackContext context)
+    {
+        if (numberOfPoints>0)
+        {
+            Debug.Log("InputAction Grappling");
+
+            lineRenderer.SetPosition(0, tf.position);
+            lineRenderer.SetPosition(1, positions[indexPoint]);
+
+            distanceJoint.connectedAnchor = positions[indexPoint];
+            distanceJoint.enabled = true;
+            lineRenderer.enabled = true;
+        }
+    }
+
+    public void ReleaseGrappling(InputAction.CallbackContext context)
+    {
+        if (numberOfPoints>0)
+        {
+            Debug.Log("InputAction ReleaseGrappling");
+
+            distanceJoint.enabled = false;
+            lineRenderer.enabled = false;
+        }
+    }
+
+
+    public void Spring(InputAction.CallbackContext context)
+    {
+        if (numberOfPoints>0)
+        {
+            Debug.Log("InputAction Spring");
+
+            distanceJoint.enabled = false;
+            lineRenderer.enabled = true;
+            lineRenderer.SetPosition(0, tf.position);
+            lineRenderer.SetPosition(1, positions[indexPoint]);
+
+            springJoint.connectedAnchor = positions[indexPoint];
+            springJoint.distance = (tf.position - cursorTf.position).magnitude / 4;
+            springJoint.frequency = speedForSpring;
+            springJoint.enabled = true;
+        }
+    }
+
+    public void ReleaseSpring(InputAction.CallbackContext context)
+    {
+        if (numberOfPoints>0)
+        {
+            Debug.Log("InputAction ReleaseSpring");
+
+            distanceJoint.enabled = false;
+            lineRenderer.enabled = false;
+            springJoint.enabled = false;
+        }
+    }
+
+
+    public void ChangeTarget(InputAction.CallbackContext context)
+    {
+        if (numberOfPoints>0)
+        {
+            if (indexPoint + 1 == numberOfPoints)
+            {
+                indexPoint = 0;
+            }
+            else
+            {
+                indexPoint += 1;
+            }
+
+            Debug.Log("inputaction; index point = " + indexPoint + " and positions[indexPoint] = " + positions[indexPoint]);
+
+            cursorTf.position = positions[indexPoint] + new Vector2(0,aboveY);
+        
+            Debug.Log("finished changeTarget");
+        }
+    }
+}
