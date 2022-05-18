99c99b8459d085f3aa9daad1009c1a1db9cd977f
Basile <bourgue.basile@gmail.com>
2022-05-18 11:31:08 +0200

    both players have to stand on a door to continue

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -5,12 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class Door : MonoBehaviour
 {
+    private bool onePlayerPresent = false;
     public Scenes nextScene; //On peut essayer de sérialiser, le but c'est de choisir vers quelle scène envoie la porte
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Manchou" || collision.gameObject.tag == "Pipoulpe")
+        if (onePlayerPresent == true)
         {
             SceneManager.LoadScene(nextScene.ToString());
         }
+        else
+        {
+            onePlayerPresent = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        onePlayerPresent = false;
     }
 }
