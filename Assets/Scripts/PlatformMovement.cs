ce07765323d7e4575519d868bff1926aeadcf308
Ayris2 <celine78.kruse@gmail.com>
2022-05-18 19:52:38 +0200

    platform verticale

--- Assets/Scripts/PlatformMovement.cs
+++ Assets/Scripts/PlatformMovement.cs
@@ -7,6 +7,7 @@ public class PlatformMovement : MonoBehaviour
     [SerializeField] public float speed;
     [SerializeField] GameObject target1; //1er objet vers lequel se dirige la plateforme 
     [SerializeField] GameObject target2; //2e objet vers lequel se dirige la plateforme quand elle a atteint la 1ère
+    [SerializeField] bool direction;
     private Vector2 target1Position;
     private Vector2 target2Position;
     private Vector2 targetPos; //cette valeur est amenée à alterner entre target1 et target2
@@ -21,15 +22,30 @@ public class PlatformMovement : MonoBehaviour
 
     void Update()
     {
-        if (targetPos.x == transform.position.x && targetPos == target1Position) //quand la plateforme atteint la target à gauche elle switch vers la droite
+        if (direction)
         {
-            targetPos = target2Position;
+            if (targetPos.x == transform.position.x && targetPos == target1Position) //quand la plateforme atteint la target à gauche elle switch vers la droite
+            {
+                targetPos = target2Position;
+            }
+            if (targetPos.x == transform.position.x && targetPos == target2Position) //quand la plateforme atteint la target à droite elle switch vers la gauche
+            {
+                targetPos = target1Position;
+            }
+            transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
         }
-        if (targetPos.x == transform.position.x && targetPos == target2Position) //quand la plateforme atteint la target à droite elle switch vers la gauche
+        if (!direction)
         {
-            targetPos = target1Position;
+            if (targetPos.y == transform.position.y && targetPos == target1Position) //quand la plateforme atteint la target à gauche elle switch vers la droite
+            {
+                targetPos = target2Position;
+            }
+            if (targetPos.y == transform.position.y && targetPos == target2Position) //quand la plateforme atteint la target à droite elle switch vers la gauche
+            {
+                targetPos = target1Position;
+            }
+            transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
         }
-        transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
     }
 
     public void launchPlatform()
